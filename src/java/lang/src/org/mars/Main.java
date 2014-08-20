4d50739b5fba051b0137ffd5d02b0289bb40b2bb
Junjie Mars <junjiemars@gmail.com>
2014-08-20 14:55:41 +0800

    renew eclipse's files

---
+++ src/java/lang/src/org/mars/Main.java
@@ -0,0 +1,31 @@
+package org.mars;
+import static java.lang.System.out;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+	public static final void main(String[] args) {
+		double f0 = 1.0f;
+		if (1.0f == f0)
+			out.println("1.0f equal");
+		else 
+			out.println("not equal");
+
+		if (1.000000001f == f0) 
+			out.println("1.0000000f equal");
+		else
+			out.println("not equal");
+
+		out.println(1.00000001f - f0);
+		if (1.11f - f0 > 0.1f) 
+			out.println("1.11f-f0>0.1f");
+		/*
+		String[] ss = new String[]{"A","B","C"};
+		List<String> l = Arrays.asList(ss);
+		for (String s : l) 
+			out.println(s);
+		*/
+	}
+
+}
