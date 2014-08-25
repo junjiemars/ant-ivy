f095cf712b5248716e0a5258f9f5740ba0e19c11
Junjie Mars <junjiemars@gmail.com>
2014-08-25 15:44:37 +0800

    javacc tutorial is works

--- src/java/redis/src/org/mars/Main.java
+++ src/java/redis/src/org/mars/Main.java
@@ -1,21 +1,13 @@
 package org.mars;
 import static java.lang.System.out;
 
-import java.util.Set;
-
-import redis.clients.jedis.Jedis;
 
 public class Main {
 	public static final void main(String[] args) {
 		out.println("Jedis");
 		
-		Jedis r = new Jedis("as0", 6379, 100);
-		r.connect();
-		Set<byte[]> s = r.keys("*".getBytes());
-		for (byte[] b : s) {
-			out.println(new String(b));
-		}
-		r.close();
+		NativeJedis.run();
+//		SpringDb.run();
 	}
 
 }
