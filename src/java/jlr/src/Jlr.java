f96553492071f7d105a74aadc49ae82fc993bca8
junjiemars <junjiemars@gmail.com>
2014-08-30 01:32:35 +0800

    antlr just works

---
+++ src/java/jlr/src/Jlr.java
@@ -0,0 +1,15 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import static java.lang.System.out;
+
+public final class Jlr {
+	public static void main(String[] args) throws Exception {
+		ANTLRInputStream in = new ANTLRInputStream(System.in);
+		CalcLexer l = new CalcLexer(in);
+		CommonTokenStream t = new CommonTokenStream(l);
+		CalcParser p = new CalcParser(t);
+
+		ParseTree tree = p.r();
+		out.println(tree.toStringTree(p));
+	}
+}
